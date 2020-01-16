09f8f56e6e67ca91b0ad4cddfc48a36d637c9e31
Malena Rey <malerey@male.5d640511d691.sn.mynetname.net>
2020-01-15 21:16:53 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+### Ejercicio clase 62, parte 2 
+
+- Al igual que hicimos con la seccion "Our clients", ahora vamos a reproducir la seccion "Services" del TP1, pero esta vez usando JavaScript. 
+
+- En el archivo main.js hay un array de objetos que corresponden con las tarjetas de la seccion Services. La idea es recorrer el array y, usando la propiedad innerHTML, agregar cada tarjeta a nuestra web. 
+
+- Es obligatorio que inserten los iconos. Para ello deberan seguir las indicaciones
+de la web de ionicons (https://ionicons.com/). No dejen de escribirme si tienen dificultades con esto. El color de cada icono esta en el objeto: recuerden que desde javascript no podemos modificar el CSS, pero si el HTML. (Recuerden que hay una manera de modificar estilos desde HTML directamente!)
+
---
+++ main.js
@@ -0,0 +1,47 @@
+
+
+const cards = [
+  {
+    title: "Lorem Ipsum",
+    description: "Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident",
+    icon: "ion-ios-analytics-outline",
+    iconColor: "#ff689b"
+  },
+  {
+    title: "Dolor Sitema",
+    description: "Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata",
+    icon: "ion-ios-bookmarks-outline",
+    iconColor: "#e9bf06"
+  },
+  {
+    title: "Sed ut perspiciatis",
+    description: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur",
+    icon: "ion-ios-paper-outline",
+    iconColor: "#3fcdc7"
+  },
+  {
+    title: "Magni Dolores",
+    description: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+    icon: "ion-ios-speedometer-outline",
+    iconColor: "#41cf2e"
+  },
+  {
+    title: "Nemo Enim",
+    description: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque",
+    icon: "ion-ios-world-outline",
+    iconColor: "#d6ff22"
+  },
+  {
+    title: "Eiusmod Tempor",
+    description: "Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi",
+    icon: "ion-ios-clock-outline",
+    iconColor: "#4680ff"
+  }
+]
+
+  
+  
+  
+  
+  
+  
